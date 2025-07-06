d3d22936936dea06914f84cbc67fcc14b4ba0f86
Nhan Doan <nhdoan@ucsd.edu>
2025-07-06 16:50:22 -0700

    TestSample

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-import fitz  # PyMuPDF
+import pymupdf  # PyMuPDF
 
 
 def extract_text_from_pdf(pdf_path: str) -> str:
-    doc = fitz.open(pdf_path)
+    doc = pymupdf.open(pdf_path)
     text = ""
     for page in doc:
         text += page.get_text()
